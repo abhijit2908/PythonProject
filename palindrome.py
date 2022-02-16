5aff0f73af08efb3769768089a55c9e8b6dcc9af
Abhijit Ghosh <abhijitghosh@Abhijits-MacBook-Air.local>
2022-02-15 23:06:15 -0500

    second function Palindrome

--- palindrome.py
+++ palindrome.py
@@ -7,3 +7,17 @@ if palindrome(word):
     print("'{}' is a Palindrome".format(word))
 else:
     print("'{}' is not a Palindrome".format(word))
+
+
+def palindrome2(x):
+    for i in range(0,int(len(x)/2)):
+        if x[i].casefold() != x[len(x)-1-i].casefold():
+            return False
+    return True
+
+
+word = input("please enter a word to check if its Palindrome ")
+if palindrome2(word):
+    print("'{}' is a Palindrome".format(word))
+else:
+    print("'{}' is not a Palindrome".format(word))
