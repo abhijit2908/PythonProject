827085e90be9103f2cb39a7b65a23fb9e1aca364
Abhijit Ghosh <abhijitghosh@Abhijits-MacBook-Air.local>
2022-02-17 01:37:29 -0500

    fibonacci

---
+++ fibonacci.py
@@ -0,0 +1,17 @@
+def fibonacci(x):
+    """Return the 'n'th fibonacci number"""
+    if 0 <= x <= 1:
+        print(x)
+        return x
+    a = 0
+    b = 1
+    for v in range(x-1):
+        v = a + b
+        b = a
+        a = v
+        print(v)
+    return v
+
+fibonacci(10)
+fibonacci(0)
+
